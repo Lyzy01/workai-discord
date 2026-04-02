f3a2506b657d16eba771a1f3e8aa90be02c97a66
Lyzy <kimmendezfeb01@gmail.com>
2026-04-02 10:38:41 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "wchatbot",
+  "version": "1.0.0",
+  "description": "A feature-rich Discord bot powered by Groq AI",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.14.1",
+    "groq-sdk": "^0.3.3"
+  }
